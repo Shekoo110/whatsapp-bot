5e04278dbdc01ba3853f33b79e8d90f4cf97fa43
shekoo110 <alshekh989@gmail.com>
2026-05-26 07:46:50 +0300

    first upload

---
+++ index.js
@@ -0,0 +1,158 @@
+const {
+    default: makeWASocket,
+    useMultiFileAuthState,
+    DisconnectReason
+} = require("@whiskeysockets/baileys")
+
+const fs = require("fs")
+const path = require("path")
+const P = require("pino")
+
+async function startBot() {
+
+    const { state, saveCreds } = await useMultiFileAuthState("session")
+
+    const sock = makeWASocket({
+        auth: state,
+        logger: P({ level: "silent" })
+    })
+
+    sock.ev.on("creds.update", saveCreds)
+
+    sock.ev.on("connection.update", ({ connection, lastDisconnect, qr }) => {
+
+        if (qr) {
+            console.log("امسح QR من واتساب")
+        }
+
+        if (connection === "open") {
+            console.log("تم تشغيل البوت")
+        }
+
+        if (connection === "close") {
+            const shouldReconnect =
+                lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut
+
+            if (shouldReconnect) {
+                startBot()
+            }
+        }
+    })
+
+    sock.ev.on("messages.upsert", async ({ messages }) => {
+
+        const msg = messages[0]
+
+        if (!msg.message) return
+
+        const text =
+            msg.message.conversation ||
+            msg.message.extendedTextMessage?.text
+
+        if (text === ".صوت") {
+
+            const folderPath = "./audio"
+
+            if (!fs.existsSync(folderPath)) {
+                fs.mkdirSync(folderPath)
+            }
+
+            const files = fs.readdirSync(folderPath)
+
+            if (files.length === 0) {
+
+                await sock.sendMessage(
+                    msg.key.remoteJid,
+                    {
+                        text: "لا توجد صوتيات"
+                    }
+                )
+
+                return
+            }
+
+            const randomAudio =
+                files[Math.floor(Math.random() * files.length)]
+
+            const audioPath =
+                path.join(folderPath, randomAudio)
+
+            await sock.sendMessage(
+                msg.key.remoteJid,
+                {
+                    audio: fs.readFileSync(audioPath),
+                    mimetype: "audio/mpeg",
+                    ptt: false
+                }
+            )
+        }
+if (text === ".صوره") {
+
+    const folderPath = "./images"
+
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath)
+    }
+
+    const files = fs.readdirSync(folderPath)
+
+    if (files.length === 0) {
+
+        await sock.sendMessage(
+            msg.key.remoteJid,
+            {
+                text: "لا توجد صور"
+            }
+        )
+
+        return
+    }
+
+    const randomImage =
+        files[Math.floor(Math.random() * files.length)]
+
+    const imagePath =
+        path.join(folderPath, randomImage)
+
+    await sock.sendMessage(
+        msg.key.remoteJid,
+        {
+            image: fs.readFileSync(imagePath),
+            caption: "صورة"
+        }
+    )
+}
+if (text === ".اصوات") {
+
+    const folderPath = "./sounds"
+
+    const files = fs.readdirSync(folderPath)
+
+    if (files.length === 0) {
+
+        await sock.sendMessage(msg.key.remoteJid, {
+            text: "لا توجد صوتيات"
+        })
+
+        return
+    }
+
+    const randomAudio =
+        files[Math.floor(Math.random() * files.length)]
+
+    const audioPath =
+        path.join(folderPath, randomAudio)
+
+    await sock.sendMessage(msg.key.remoteJid, {
+        audio: fs.readFileSync(audioPath),
+        mimetype: "audio/mpeg",
+        ptt: false
+    });
+
+}
+
+    })
+
+}
+
+startBot()
