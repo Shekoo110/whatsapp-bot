ac6faff3542a52c48ce355092019ba3f2b7afa76
Shekoo110 <150535160+Shekoo110@users.noreply.github.com>
2026-05-26 23:04:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(Server running on port ${PORT})
+    console.log(`Server running on port ${PORT}`)
 
 // ====== Start Bot ======
 async function startBot() {
